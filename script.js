ad12f961e1fb92b9d126599022be6c6ada2e3588
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-12-03 11:16:53 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@";
+let novaSenha = "";
+
+sizePassword.innerHTML =sliderElement.value;
