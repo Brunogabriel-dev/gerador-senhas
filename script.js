7b4fe920ee2214f178aa6890aded61669d412684
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-12-03 21:41:19 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
@@ -7,29 +6,30 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@";
-let novaSenha = "";
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
 
-sizePassword.innerHTML =sliderElement.value;
+sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function() {
   sizePassword.innerHTML = this.value;
 }
 
-function generationPassword() {
+
+function generatePassword(){
+
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
   
-  let pass ="";
-
-  for(let i = 0, n = charset.length; i < sliderElement.value;
- ++i){
-  pass += charset.charAt(Math.floor(Math.random() * n));
- }
-
- containerPassword.classList.remove("hide");
- password.innerHTML = pass;
-
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
 }
 
 function copyPassword(){
-  alert("Senha copiada com sucesso")
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
 }
