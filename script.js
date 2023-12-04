1940efd03373d6a253552c9ce6e7b3bceb86df32
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-12-03 21:15:36 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ sizePassword.innerHTML =sliderElement.value;
 slider.oninput = function(){
   sizePassword.innerHTML = this.value;
 }
+
+function generationPassword() {
+  
+  let pass ="";
+
+  for(let i = 0, n = charset.length; i < sliderElement.value;
+ ++i){
+  pass += charset.charAt(Math.floor(Math.random() * n));
+ }
+
+ containerPassword.classList.remove("hide");
+ password.innerHTML = pass;
+
+}
+
+function copyPassword(){
+  alert("Senha copiada com sucesso")
+}
